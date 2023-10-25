3a2aa93e75d97213a83c2fb90ab67267739e52d5
yeahright711 <ramonmora711@gmail.com>
2023-10-24 18:09:32 -0700

    initial push

---
+++ code/StockData.py
@@ -0,0 +1,33 @@
+import csv
+
+import time
+from datetime import datetime
+
+
+class StockData:
+    def __init__(self, path):
+        self.path = path
+        self.data = None
+
+    def load(self):
+        """A function to assing the csv dataset to a list of lists.
+        Does not include headers.
+
+        All data is expected to be a string
+        """
+        # to keep us in compliance with EU standards, we must log the datetime
+        # of all data loads
+        epoch = time.time()
+        self._date = datetime.utcfromtimestamp(epoch).\
+            strftime('%Y-%m-%d %H:%M:%S')
+        print("DATA LOADED AT", self._date)
+
+        # open data using csv reader
+        with open(self.path, newline='') as file:
+            reader = csv.reader(file)
+            # skip header row
+            next(reader, None)
+            # append list of strings into list
+            self.data = []
+            for row in reader:
+                self.data.append(row)
