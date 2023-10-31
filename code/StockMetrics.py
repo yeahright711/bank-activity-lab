1d399197fa6fbf8b99e8f8878dd7aea2985cc477
yeahright711 <ramonmora711@gmail.com>
2023-10-30 17:14:03 -0700

    almost done

--- code/StockMetrics.py
+++ code/StockMetrics.py
@@ -1,6 +1,5 @@
 
 import statistics as stats
-
 from code.StockData import StockData
 
 
@@ -18,17 +17,56 @@ class StockMetrics(StockData):
         """
         averages = []
         for row in self.data:
-            for val in row:
-                print(val)
+             valid_prices = []
+             for num in row[1:]:
+                 num = num.strip()
+            
+                 if num and num.replace('.', '', 1).isdigit():
+                     price = float(num)
+                     valid_prices.append(price)
+             print(valid_prices)
 
+             sum_averages = sum(valid_prices)/len(valid_prices)
+             averages.append(round(sum_averages, 3))
+             print(averages)
+               
         return averages
 
     def median02(self):
         """pt2
         """
-        ...
+        median = []
+        for row in self.data:
+             valid_prices = []
+             for num in row[1:]:
+                 num = num.strip()
+            
+                 if num and num.replace('.', '', 1).isdigit():
+                     price = float(num)
+                     valid_prices.append(price)
+             print(valid_prices)
+
+             sum_median = stats.median(valid_prices)
+             median.append(sum_median)
+             print(median)
+        return median
 
     def stddev03(self):
         """pt3
         """
-        ...
+        stddev = []
+        for row in self.data:
+             valid_prices = []
+             for num in row[1:]:
+                 num = num.strip()
+            
+                 if num and num.replace('.', '', 1).isdigit():
+                     price = float(num)
+                     valid_prices.append(price)
+             print(valid_prices)
+
+             new_stddev = stats.stdev(valid_prices)
+             stddev.append(round(new_stddev, 3))
+             print(stddev)
+
+        return stddev
